ab101bbdb28542254d1a04f1515ff1dbd99474f7
Hiago Rios <hriosdev@gmail.com>
2021-07-03 19:50:49 -0300

    feat: list manipulation and printing

--- main.pl
+++ main.pl
@@ -1,4 +1,21 @@
-% Territory definition
+% ======================= List manipulation =======================
+% Delete: removes element from list
+% delete(List, element, Result)
+
+% Append: concats the two lists
+% append(List, [element], Result)
+
+% Print List: Base case
+printList([]) :- !.
+
+% Print List: Recursion
+printList([Head | Tail]) :-
+    format('\n~w',Head),
+    printList(Tail)
+    .
+% ======================= List manipulation =======================
+
+% ====================== Territory definition =====================
 edgeDef(a, b).
 edgeDef(a, c).
 edgeDef(b, c).
@@ -24,6 +41,19 @@ edgeDef(m, q).
 edgeDef(n, o).
 edgeDef(n, q).
 edgeDef(p, r).
+% ====================== Territory definition =====================
 
 % Bidirectionally checking for edge
 edge(X, Y) :- (edgeDef(X, Y), !; edgeDef(Y, X)), !.
+
+% Prints all color lists
+printColors(Red, Blue, Yellow, Green) :- 
+    writeln('red'),
+    printList(Red),
+    writeln('blue'),
+    printList(Blue),
+    writeln('yellow'),
+    printList(Yellow),
+    writeln('green'),
+    printList(Green)
+    .
